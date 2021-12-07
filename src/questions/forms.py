2489929b79ebad2d6a0087381e9ce7c2573fb3b1
Garmirian <kevinbalian@protonmail.com>
2021-12-07 17:14:19 +0100

    css

--- src/questions/forms.py
+++ src/questions/forms.py
@@ -21,8 +21,8 @@ class ProjectChoices(forms.Form):
     pac = forms.BooleanField(label="Pompe à chaleur")
     iso = forms.BooleanField(label="Isolation des combles")
 
-POWER_LEVEL = [("three", "3 kWc"), ("four_five", "4,5 kWc")]
-PV_TYPE = [("classic", "Classique"), ("hybrid", "Hybrides")]
+POWER_LEVEL = [("three", "3"), ("four_five", "4,5"), ("six", "6"), ("seven_five", "7,5"), ("nine", "9"), ("ten_five", "10,5"), ("twelve", "12")]
+PV_TYPE = [("classic", ""), ("hybrid", "")]
 
 class CharacteristicsPv(forms.Form):
     power = forms.ChoiceField(choices=POWER_LEVEL, label="Dimensionnement de l'installation (kWc)", widget=forms.RadioSelect)
