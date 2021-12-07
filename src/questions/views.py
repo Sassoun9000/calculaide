2fd30992cb092a0d5c021057501ddbbf683c15eb
Sassoun <headshooter69@free.fr>
2021-12-08 00:16:10 +0100

    Question path

--- src/questions/views.py
+++ src/questions/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from .forms import *
 
+from .utils import QPATH_FORMS
+
 
 def index(request):
     return render(request, "index.html")
@@ -14,3 +16,40 @@ def general_infos(request):
         "pv_characteristics": PVCharacteristics()
     }
     return render(request, "general_infos.html", context=context)
+
+
+def questions(request):
+    context = dict()
+    if request.method == "POST":
+        r = request.POST
+        print(r)
+        if "previous" in r:
+            request.session["index"] -= 2
+            print("YOOOOP")
+        current_index = request.session["index"]
+        if current_index == 0:
+            request.session["QPath"] = ["start",]
+            if "pv" in r:
+                request.session["QPath"].append(QPATH_FORMS["pv"])
+            if "pac" in r:
+                request.session["QPath"].append(QPATH_FORMS["pac"])
+            if "iso" in r:
+                request.session["QPath"].append(QPATH_FORMS["iso"])
+            request.session["QPath"].append("ColorCategory()")
+
+        request.session["index"] += 1
+        i = request.session["index"]
+
+        context["form"] = eval(request.session["QPath"][i])
+        if i == 1:
+            context["2nd_question"] = True
+        if i == len(request.session["QPath"]):
+            return render(request, "results.html")
+
+    else:
+        request.session["index"] = 0
+        context["form"] = ProjectChoices()
+        context["first_question"] = True
+        print("Première itération")
+
+    return render(request, "questions.html", context=context)
