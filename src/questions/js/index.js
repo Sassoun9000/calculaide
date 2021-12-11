316515a7953288c46883496cffb3e34f0dc27832
Garmirian <kevinbalian@protonmail.com>
2021-12-11 14:54:01 +0100

    css html

--- src/questions/js/index.js
+++ src/questions/js/index.js
@@ -1,9 +1,9 @@
 function suivantBtn() {
-    document.getElementById("form_nav").style.transform = "translate(-300px)";
+    document.getElementById("form_nav").style.border = "10px solid red";
 }
 
 document.getElementById("id_family_members_0").addEventListener("click", update())
 
+document.getElementsByClassName("previous").addEventListener("load", suivantBtn())
 
-document.querySelector("form_product").addEventListener("load", suivantBtn())
-
+console.log("coucou")
