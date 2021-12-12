69e3d68311e1d26772d7e88e1df0aca2cff2b50c
Garmirian <kevinbalian@protonmail.com>
2021-12-12 15:20:46 +0100

    css html js "prvious button"

--- src/questions/js/index.js
+++ src/questions/js/index.js
@@ -7,14 +7,12 @@ aides = {
     "6": ["39405", "50511", "77825", ">77825"],
     "7": ["43817", "56162", "86569", ">86569"],
     "8": ["48229", "61813", "95313", ">95313"],
-}
+  }
 
-function suivantBtn() {
-    document.getElementById("form_nav").style.border = "10px solid red";
-}
+  function suivantBtn() {
+      document.getElementById("form_nav").style.border = "10px solid red";
+  }
 
-document.getElementById("id_family_members_0").addEventListener("click", update())
+  document.getElementById("id_family_members_0").addEventListener("click", update())
 
-document.getElementsByClassName("previous").addEventListener("load", suivantBtn())
-
-console.log("coucou")
+  document.getElementsByClassName("previous").addEventListener("load", suivantBtn())
