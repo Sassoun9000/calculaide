c18abe821513d7505decc7cf60b1f8ab6a56bab6
Garmirian <kevinbalian@protonmail.com>
2022-01-03 16:06:21 +0100

    css label logo

--- src/questions/forms.py
+++ src/questions/forms.py
@@ -50,7 +50,7 @@ PV_POWER_LEVEL = [
     ("12", "12"),
     ("15", "15"),
 ]
-PV_TYPE = [("classic", ""), ("hybrides", "")]
+PV_TYPE = [("classic", "Classiques"), ("hybrides", "Hybrides")]
 
 
 class PVCharacteristics(forms.Form):
